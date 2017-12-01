90f8154dfbd2aabe774c0c2c4ecc3f9f014d6a2c
Jorge Mendes <jorge.mendesx@gmail.com>
2017-12-01 11:20:25 -0200

    login no sistema

--- index.php
+++ index.php
@@ -1,16 +1,85 @@
 <?php
+session_start();
 
-require_once "conecta.php";
-require_once "consultarPessoa.php";
-
-foreach ($detalhesPessoa as $pessoa) {
-
-    echo ucfirst(strtolower($pessoa->nome));
-    echo ucfirst(strtolower($pessoa->email));
-    echo ucfirst(strtolower($pessoa->senha));
-    echo ucfirst(strtolower($pessoa->perfil));
-
+if($_SESSION) {
+    if(array_key_exists('logado', $_SESSION)) {
+        header("Location: dashboard.php");
+    }
 }
-
-
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+<h1>Tela de Login</h1>
+<div class="container-fluid centered" >
+    <div class="container">
+        <form class="form-horizontal">
+            <div class="form-group">
+                <label for="email" class="col-sm-2 control-label">Usuário</label>
+
+                <div class="col-sm-2">
+                    <input class="form-control" id="email" name="email" placeholder="Digite seu usuário">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="senha" class="col-sm-2 control-label">Senha</label>
+                <div class="col-sm-2">
+                    <input type="password" maxlength="6" class="form-control" id="senha" name="senha" placeholder="Digite sua senha">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-2">
+                    <h6>Escolha o perfil de usuario</h6>
+                    <div class="checkbox">
+                        <select class="form-horizontal" id="perfil">
+                            <option value="volvo">Admin</option>
+                            <option value="usu">basico</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-2">
+                    <button type="button" class="btn btn-default" id="login">Entrar</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+<script>
+    $(document).ready(function(){
+
+        $("#login").click(() => {
+            let email = $('#email').val();
+            let senha = $('#senha').val();
+
+            if( email !== "" && senha !== "" ) {
+                $.ajax({
+                    method: "POST",
+                    url: "login_session.php",
+                    data: { email: email, senha: senha }
+                }).done(function( data ) {
+                    if(data.status !== 200 ) {
+                        alert(`${data.message}`);
+                    } else {
+                        alert(`Seja bem bindo ao sistemas do Ganço e do Wiliiam`);
+                        window.location = "dashboard.php";
+                    }
+                });
+            } else {
+                console.error('BLA');
+            }
+        });
+    });
+</script>
+</body>
+</html>
---
+++ login_session.php
@@ -0,0 +1,49 @@
+<?php
+
+require_once "conecta.php";
+
+header('Content-Type: application/json');
+
+session_start();
+
+$msg = ['results' => [
+    'currect' => [
+        'status' => 200
+    ],
+    'error' => [
+        'message' => 'Usuário ou senha inválidos!',
+        'status' => 500
+    ]
+]];
+
+if ($_POST) {
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    $pdo = conectar();
+
+    $loginPessoa = $pdo->prepare("SELECT * FROM pessoa WHERE email = :email AND senha = :senha");
+
+
+    $loginPessoa->bindParam(":email", $email, PDO::PARAM_STR);
+    $loginPessoa->bindParam(":senha", $senha, PDO::PARAM_STR);
+
+    $loginPessoa->execute();
+
+    $login = $loginPessoa->fetch(PDO::FETCH_ASSOC);
+
+    if ($login) {
+
+        $_SESSION['email'] = $email;
+        $_SESSION['logado'] = true;
+
+        echo json_encode($msg['results']['currect']);
+
+    } else {
+
+        session_destroy();
+
+        echo json_encode($msg['results']['error']);
+    }
+}
+
