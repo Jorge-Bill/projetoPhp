edeeb9d57bb59ae373b2bbf2ff27b6499f2b4241
Jorge Mendes <jorge.mendesx@gmail.com>
2017-12-04 18:28:40 -0200

    imagem no cadastro

--- paginas/cadastrarPessoa.php
+++ paginas/cadastrarPessoa.php
@@ -7,34 +7,51 @@ $nome       = (isset($_REQUEST['nomePessoa'])) ? $_REQUEST['nomePessoa'] : '';
 $email      = (isset($_REQUEST['emailPessoa'])) ? $_REQUEST['emailPessoa'] : '';
 $senha      = (isset($_REQUEST['senhaPessoa'])) ? $_REQUEST['senhaPessoa'] : '';
 $perfil     = (isset($_REQUEST['perfilPessoa'])) ? $_REQUEST['perfilPessoa'] : '';
-$foto       = (isset($_FILES['imagemPessoa'])) && $_FILES['imagemPessoa']['size'] ? $_FILES['imagemPessoa'] : '';
 
-if(!file_exists("imagens")):
-    mkdir("../imagens");
+
+$foto       = (isset($_FILES['imagemPessoa'])) && $_FILES['imagemPessoa']['size'] ? $_FILES['imagemPessoa'] : '';
+$nomeFoto   = $foto['name'];
+$tipo       = $foto['type'];
+$tamanho    = $foto['size'];
+$destino    = '../imagens/';
+
+var_dump($foto);
+var_dump($nomeFoto);
+var_dump($tipo);
+var_dump($tamanho);
+var_dump($destino);
+
+$uploadfile = $destino . basename($_FILES['imagemPessoa']['name']);
+
+if(!preg_match('/^image\/(pjpeg|jpeg|png|gif|bmp)$/', $tipo))
+{
+    print_r ('Isso não é uma imagem válida');
+    exit;
+}
+
+if(!file_exists($destino)):
+    mkdir($destino);
 endif;
 
-// Monta o caminho de destino com o nome do arquivo
-$nome_foto = date('dmY') . '_' . $_FILES['imagemPessoa']['nomePessoa'];
 
-// Essa função move_uploaded_file() copia e verifica se o arquivo enviado foi copiado com sucesso para o destino
-if (!move_uploaded_file($_FILES['imagemPessoa']['tmp_name'], '../imagens/'.$nome_foto)):
-    echo "Houve um erro ao gravar arquivo na pasta de destino!";
+if (!move_uploaded_file($_FILES['imagemPessoa']['tmp_name'], $uploadfile)):
+    print_r( "Houve um erro ao gravar arquivo na pasta de destino!");
 endif;
 
 $pdo = conectar();
 
-$sql = ("INSERT INTO pessoa  (nome, foto, perfil, senha, email) VALUES (:nome, :foto, :perfil, :senha, :email)");
+$sql = ("INSERT INTO pessoa (nome, foto, perfil, senha, email) VALUES (:nome, :foto, :perfil, :senha, :email)");
 
 $cadastrarPessoa = $pdo->prepare($sql);
 
-$cadastrarPessoa->bindParam(":nome", $nome, PDO::PARAM_STR);
-$cadastrarPessoa->bindParam(":foto", $foto, PDO::PARAM_STR);
-$cadastrarPessoa->bindParam(":perfil", $perfil, PDO::PARAM_STR);
-$cadastrarPessoa->bindParam(":senha", $senha, PDO::PARAM_STR);
-$cadastrarPessoa->bindParam(":email", $email, PDO::PARAM_STR);
+$cadastrarPessoa->bindParam(":nome", $nome);
+$cadastrarPessoa->bindParam(":foto", $nomeFoto);
+$cadastrarPessoa->bindParam(":perfil", $perfil);
+$cadastrarPessoa->bindParam(":senha", $senha);
+$cadastrarPessoa->bindParam(":email", $email);
 
 if ($cadastrarPessoa->execute()){
-        header('Location: listaUsuarios.php');
+    header('Location: listaUsuarios.php');
 }
 else{
     echo "Erro ao cadastrar";
