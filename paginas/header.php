6590adbbe1dba0174ec977c9d9c66f54e28edbc0
Jorge Mendes <jorge.mendesx@gmail.com>
2017-12-04 11:15:53 -0200

    ajustes menu topo

--- paginas/header.php
+++ paginas/header.php
@@ -18,19 +18,26 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Brand</a>
+            <a class="navbar-brand glyphicon glyphicon-flash
+" href="#"></a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="dashboard.php">Dashboard</a></li>
-                <li><a href="listaUsuarios.php">Usuarios</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Usuarios <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="listaUsuarios.php">Listar usuarios</a></li>
+                        <li><a href="form-cadastro.php">Cadastrar usuario</a></li>
+                        <li><a href="form-cadastro.php">Editar usuario</a></li>
+                    </ul>
+                </li>
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Opções <span class="caret"></span></a>
                     <ul class="dropdown-menu">
                         <li><p>Olá <?php  ?></p></li>
-                        <li><a href="form-cadastro.php">Cadastrar usuario</a></li>
                         <li><a href="perfilUsuario.php">Perfil</a></li>
                         <li role="separator" class="divider"></li>
                         <li><a href="logout.php">Sair</a></li>
