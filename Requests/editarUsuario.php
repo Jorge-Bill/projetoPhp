4a6b2e706fa1c6e83469c12e583060cc70395ad2
Jorge Mendes <jorge.mendesx@gmail.com>
2017-12-20 12:26:20 -0200

    editar perfil tratando a imagem

---
+++ Requests/editarUsuario.php
@@ -0,0 +1,84 @@
+<?php
+
+require_once "../paginas/conecta.php";
+
+header('Content-Type: application/json');
+
+$msg = ['results' => [
+    'currect' => [
+        'message' => 'funciona!',
+        'status' => 200
+    ],
+    'error' => [
+        'message' => 'Erro ao editar!',
+        'status' => 500
+    ]
+]];
+
+    if ($_POST){
+        $pessoa = (object)[
+            'id'        => $_POST['id'],
+            'nome'      => $_POST['nome'],
+            'email'     => $_POST['email'],
+            'senha'     => $_POST['senha'],
+            'perfil'    => $_POST['perfil']
+        ];
+
+        if(array_key_exists('foto', $_FILES)) {
+            $pessoa->foto = $_FILES['foto'];
+        }
+
+        if (!empty($pessoa->foto)){
+
+            $destino = '../imagens/';
+
+            $uploadfile = $destino . basename($pessoa->foto['name']);
+
+            if(!preg_match('/^image\/(pjpeg|jpeg|png|gif|bmp|jpg)$/', $pessoa->foto['type'])){
+                print_r ('Isso não é uma imagem válida');
+                exit;
+            }
+
+            if(!file_exists($destino)) {
+                mkdir($destino);
+            }
+
+            if (!move_uploaded_file($pessoa->foto['tmp_name'], $uploadfile)) {
+                print_r("Houve um erro ao gravar arquivo na pasta de destino!");
+            }
+        }
+
+        $pdo = conectar();
+
+        if(array_key_exists('foto', $_FILES)) {
+            $sql = "UPDATE pessoa SET nome=:nome, foto=:foto, perfil=:perfil, senha=:senha, email=:email WHERE id=:id;";
+
+            $updatePessoa = $pdo->prepare($sql);
+
+            $updatePessoa->bindParam(":id", $pessoa->id);
+            $updatePessoa->bindParam(":nome", $pessoa->nome);
+            $updatePessoa->bindParam(":foto", $pessoa->foto['name']);
+            $updatePessoa->bindParam(":perfil", $pessoa->perfil);
+            $updatePessoa->bindParam(":senha", $pessoa->senha);
+            $updatePessoa->bindParam(":email", $pessoa->email);
+        } else {
+            $sql = "UPDATE pessoa SET nome=:nome, perfil=:perfil, senha=:senha, email=:email WHERE id=:id;";
+
+            $updatePessoa = $pdo->prepare($sql);
+
+            $updatePessoa->bindParam(":id", $pessoa->id);
+            $updatePessoa->bindParam(":nome", $pessoa->nome);
+            $updatePessoa->bindParam(":perfil", $pessoa->perfil);
+            $updatePessoa->bindParam(":senha", $pessoa->senha);
+            $updatePessoa->bindParam(":email", $pessoa->email);
+        }
+
+
+        if ($updatePessoa->execute()){
+            echo json_encode($msg['results']['currect']);
+        }
+        else{
+            echo json_encode($msg['results']['error']);
+        }
+    }
+?>
--- paginas/editarForm.php
+++ paginas/editarForm.php
@@ -9,47 +9,45 @@ $editarPessoa = $pdo->prepare('SELECT * FROM pessoa WHERE id = :id');
 $editarPessoa->bindParam(":id", $id);
 
 
-if ($editarPessoa->execute()){
-$editarPessoa = $editarPessoa->fetch(PDO::FETCH_ASSOC);
-}
-else{
-echo "Erro ao cadastrar";
-print_r($editarPessoa->errorInfo());
+if ($editarPessoa->execute()) {
+    $editarPessoa = $editarPessoa->fetch(PDO::FETCH_ASSOC);
+} else {
+    echo "Erro ao cadastrar";
+    print_r($editarPessoa->errorInfo());
 }
 
 ?>
+<script src="/js/jquery.validate.min.js"></script>
 
 <div class="col-md-7 col-md-offset-2 col-xs-12">
     <h1 class="text-center">Editar</h1>
-    <form>
+    <form id="formEditar">
         <div class="form-group">
             <!--            <label for="id">Id</label>-->
-            <input class="form-control" type='hidden' name='idPessoa' id="idPessoa" value="<?php echo $editarPessoa['id']?>">
+            <input class="form-control" type='hidden' name='idPessoa' id="idPessoa"
+                   value="<?php echo $editarPessoa['id'] ?>">
         </div>
         <div class="form-group">
             <label for="nomePessoa">Nome</label>
-            <input type="text" value="<?php echo $editarPessoa['nome']?>" class="form-control" id="nomePessoa" name="nomePessoa" placeholder="Nome" required >
+            <input type="text" value="<?php echo $editarPessoa['nome'] ?>" class="form-control valid" id="nomePessoa"
+                   name="nomePessoa" placeholder="Nome" minlength="2" required>
         </div>
         <div class="form-group">
             <label for="emailPessoa">E-mail</label>
-            <input type="email" value="<?php echo $editarPessoa['email']?>" class="form-control" id="emailPessoa" name="emailPessoa" placeholder="E-mail" required>
+            <input type="email" value="<?php echo $editarPessoa['email'] ?>" class="form-control valid" id="emailPessoa"
+                   name="emailPessoa" placeholder="E-mail" required>
         </div>
         <div class="form-group">
             <label for="senhaPessoa">Senha</label>
-            <input type="text" value="<?php echo $editarPessoa['senha']?>" class="form-control" id="senhaPessoa" name="senhaPessoa" placeholder="Senha" required>
+            <input type="text" value="<?php echo $editarPessoa['senha'] ?>" class="form-control valid" id="senhaPessoa"
+                   name="senhaPessoa" placeholder="Senha" minlength="5" required>
         </div>
 
-        <?php
-        if (!empty($editarPessoa['foto'])): ?>
-            <div class="form-group">
-                <img class="thumbnail" src="/../imagens/<?php echo $editarPessoa['foto']; ?>" width="500" height="auto" alt="">
-            </div>
-            <?php
-        else :
-            print_r("Que tal adicionar uma foto?");
-        endif;
+        <div class="form-group">
+            <img id="imgPrevia" class="thumbnail" src="/../imagens/<?php echo $editarPessoa['foto']; ?>" width="500"
+                 height="auto" alt="">
+        </div>
 
-        ?>
         <div class="form-group">
             <label for="imagemPessoa">Foto</label>
             <input type="file" name="imagemPessoa" id="imagemPessoa">
@@ -58,7 +56,8 @@ print_r($editarPessoa->errorInfo());
 
         <div class="form-group col-md-4">
             <label>Perfil</label>
-            <select value="<?php echo $editarPessoa['perfil']?>" class="form-control" id="perfilPessoa" name="perfilPessoa">
+            <select value="<?php echo $editarPessoa['perfil'] ?>" class="form-control" id="perfilPessoa"
+                    name="perfilPessoa">
                 <option>Admin</option>
                 <option>Basico</option>
             </select>
@@ -72,50 +71,86 @@ print_r($editarPessoa->errorInfo());
     </form>
 </div>
 
+
+<div class="modal fade" tabindex="-1" role="dialog" id="editarModal">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
+                </button>
+                <h4 class="modal-title">Mensagem</h4>
+            </div>
+            <div class="modal-body">
+                <p id="confirmacao"></p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+
 <script>
 
-    $(document).ready(function(){
-        let form;
+    $(document).ready(function () {
+
+        let form = new FormData();
+
+        $("#imagemPessoa").change(function () {
+            if (this.files && this.files[0]) {
+                let reader = new FileReader();
+
+                reader.onload = function (e) {
+                    $('#imgPrevia').attr('src', e.target.result);
+                };
+                reader.readAsDataURL(this.files[0]);
+            }
+        });
+
         $('#imagemPessoa').change(function (event) {
-            form = new FormData();
             form.append('foto', event.target.files[0]);
         });
 
+        $("#formEditar").validate();
         $("#editar").click(() => {
-            let id      = $('#idPessoa').val();
-            let nome    = $('#nomePessoa').val();
-            let email   = $('#emailPessoa').val();
-            let senha   = $('#senhaPessoa').val();
-            let foto    = $('#imagemPessoa').val();
-            let perfil  = $('#perfilPessoa').val();
+            let img = $("#imgPrevia").attr("src");
+            let id = $('#idPessoa').val();
+            let nome = $('#nomePessoa').val();
+            let email = $('#emailPessoa').val();
+            let senha = $('#senhaPessoa').val();
+            let foto = $('#imagemPessoa').val();
+            let perfil = $('#perfilPessoa').val();
 
-            if(nome && email && senha && perfil && foto) {
-                form.append('id', id);
-                form.append('nome', nome);
-                form.append('email', email);
-                form.append('senha', senha);
-                form.append('foto', foto);
-                form.append('perfil', perfil);
+            form.append('id', id);
+            form.append('nome', nome);
+            form.append('email', email);
+            form.append('senha', senha);
+            form.append('perfil', perfil);
+            form.append('foto', foto);
 
-                $.ajax({
-                    method: "POST",
-                    processData: false,
-                    contentType: false,
-                    url: "/navegacao.php?page=editarUsuario",
-                    data: form
-                }).then(
-                        function success(data) {
-                            window.location = "/navegacao.php?page=listaUsuarios";
-                        },
-
-                        function fail(data) {
-                            alert(`${data.message}`);
+            $.ajax({
+                method: "POST",
+                processData: false,
+                contentType: false,
+                url: "/Requests/editarUsuario.php",
+                data: form
+            })
+                .then(
+                    function success(data) {
+                        if (data.status !== 200) {
+                            $('#editarModal').modal('show');
+                            $("#confirmacao").text("Erro!");
+                        } else {
+                            $('#editarModal').modal('show');
+                            $("#confirmacao").text("Registro editado com sucesso");
+                            setInterval(function () {
+                                window.location = "/navegacao.php?page=listaUsuarios"
+                            }, 2000);
                         }
-                    );
-            }
-            else {
-                alert('Erro! preencha os campos');
-            }
+                    }
+                );
+
         });
     });
 
