c14483419c073ae40c675e5c2db00e2eea658afe
Jorge Mendes <jorge.mendesx@gmail.com>
2018-01-18 19:07:03 -0200

    remocao do link para importar o animation.css

--- paginas/header.php
+++ paginas/header.php
@@ -5,7 +5,6 @@
     <title>Ganso | William</title>
     <link rel="stylesheet" href="/../css/bootstrap.min.css">
     <link rel="stylesheet" href="/../css/estilos.css">
-    <link rel="stylesheet" href="/../css/animate.css">
 </head>
 <body>
 <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
